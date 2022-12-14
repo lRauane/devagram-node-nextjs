27a094b0264e8cc7341e4fee28cd92fe70b4e749
Rauane Lima <rauanelima7@gmail.com>
2022-12-14 14:55:02 -0300

    Fazeno upload das imagens no banco de dados

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -2,50 +2,63 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { RespostasPadraoMSG } from "../../types/respostasPadraoMSG";
 import type { CadastroRequisicao } from "../../types/CadastroRequisicao";
 import { usuarioModel } from "../../models/usuarioModel";
-import {conectarMongoDB} from '../../middlewares/conectarMongoDB'
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
 import md5 from "md5";
+import { upload, uploadImagemCosmisc } from "../../services/uploadImagemCosmic";
+import nc from "next-connect";
 
-const endpointCadastro = async (
-  req: NextApiRequest,
-  res: NextApiResponse<RespostasPadraoMSG>
-) => {
-  if (req.method == "POST") {
-    const usuario = req.body as CadastroRequisicao;
+const handler = nc()
+  .use(upload.single("file"))
+  .post(
+    async (req: NextApiRequest, res: NextApiResponse<RespostasPadraoMSG>) => {
+      const usuario = req.body as CadastroRequisicao;
 
-    if (!usuario.nome || usuario.nome.length < 2) {
-      return res.status(400).json({ erro: "Nome inválido" });
-    }
+      if (!usuario.nome || usuario.nome.length < 2) {
+        return res.status(400).json({ erro: "Nome inválido" });
+      }
 
-    if (
-      !usuario.email ||
-      usuario.email.length < 5 ||
-      !usuario.email.includes("@") ||
-      !usuario.email.includes(".")
-    ) {
-      return res.status(400).json({ erro: "Email inválido" });
-    }
+      if (
+        !usuario.email ||
+        usuario.email.length < 5 ||
+        !usuario.email.includes("@") ||
+        !usuario.email.includes(".")
+      ) {
+        return res.status(400).json({ erro: "Email inválido" });
+      }
 
-    if (!usuario.senha || usuario.senha.length < 4) {
-      return res.status(400).json({ erro: "senha inválida" });
-    }
+      if (!usuario.senha || usuario.senha.length < 4) {
+        return res.status(400).json({ erro: "senha inválida" });
+      }
 
-    // Validação se já existe usuario com o mesmo email
-    const usuarioComMesmoEmail = await usuarioModel.find({email : usuario.email});
+      // Validação se já existe usuario com o mesmo email
+      const usuarioComMesmoEmail = await usuarioModel.find({
+        email: usuario.email,
+      });
 
-    if(usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0){
-      return res.status(400).json({ erro: "Já existe uma conta com esse email! "})
-    }
+      if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
+        return res
+          .status(400)
+          .json({ erro: "Já existe uma conta com esse email! " });
+      }
 
-    //salvar no banco de dados
-    const usuarioAserSalvo = {
-      nome : usuario.nome,
-      email: usuario.email,
-      senha: md5(usuario.senha)
-    }
-    await usuarioModel.create(usuarioAserSalvo);
-    return res.status(200).json({ msg: "Usuario criado com sucesso" });
+      // enviar a imagem para o comic
+      const image = await uploadImagemCosmisc(req)
+
+      //salvar no banco de dados
+      const usuarioAserSalvo = {
+        nome: usuario.nome,
+        email: usuario.email,
+        senha: md5(usuario.senha),
+        avatar: image?.media?.url 
+      };
+      await usuarioModel.create(usuarioAserSalvo);
+      return res.status(200).json({ msg: "Usuario criado com sucesso" });
+});
+
+export const config = {
+  api:{
+    bodyParser : false
   }
-  return res.status(405).json({ erro: "Metodo informado não é válido" });
-};
+}
 
-export default conectarMongoDB(endpointCadastro);
+export default conectarMongoDB(handler);
---
+++ services/uploadImagemCosmic.ts
@@ -0,0 +1,38 @@
+import multer from "multer";
+import cosmicjs from "cosmicjs";
+
+const {
+  CHAVE_DE_GRAVACAO_AVATARES,
+  CHAVE_DE_GRAVACAO_PUBLICACOES,
+  BUCKET_AVATARES,
+  BUCKET_PUBLICACOES,
+} = process.env;
+const cosmisc = cosmicjs();
+const bucketAvatar = cosmisc.bucket({
+  slug: BUCKET_AVATARES,
+  write_key: CHAVE_DE_GRAVACAO_AVATARES,
+});
+
+const bucketPublicacoes = cosmisc.bucket({
+  slug: BUCKET_PUBLICACOES,
+  write_key: CHAVE_DE_GRAVACAO_PUBLICACOES,
+});
+
+const storage = multer.memoryStorage()
+const upload = multer({storage : storage})
+
+const uploadImagemCosmisc = async(req: any) =>{
+  if (req?.file?.originalname){
+    const media_object = {
+      originalname : req.file.originalname,
+      buffer : req.file.buffer
+    };
+    if (req.url && req.url.includes('publicacao')){
+      return await bucketPublicacoes.addMedia({media : media_object})
+    } else{
+      return await bucketAvatar.addMedia({media : media_object})
+    }
+  }
+}
+
+export {upload, uploadImagemCosmisc};

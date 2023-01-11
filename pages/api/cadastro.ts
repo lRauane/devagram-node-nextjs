7b37d6a3b4910aa70ee7cc65827086fcb55dda16
Rauane Lima <rauanelima7@gmail.com>
2023-01-11 10:20:09 -0300

    Resolvendo problemas nos avatares

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -9,57 +9,53 @@ import nc from "next-connect";
 import { PoliticaCORS } from "../../middlewares/politicaCors";
 
 const handler = nc()
-  .use(updload.single("file"))
-  .post(
-    async (req: NextApiRequest, res: NextApiResponse<RespostasPadraoMSG>) => {
-      const usuario = req.body as CadastroRequisicao;
+    .use(updload.single('file'))
+    .post(async (req : NextApiRequest, res : NextApiResponse<RespostasPadraoMSG>) => {
+        try{
+            const usuario = req.body as CadastroRequisicao;
+        
+            if(!usuario.nome || usuario.nome.length < 2){
+                return res.status(400).json({erro : 'Nome invalido'});
+            }
+    
+            if(!usuario.email || usuario.email.length < 5
+                || !usuario.email.includes('@')
+                || !usuario.email.includes('.')){
+                return res.status(400).json({erro : 'Email invalido'});
+            }
+    
+            if(!usuario.senha || usuario.senha.length < 4){
+                return res.status(400).json({erro : 'Senha invalida'});
+            }
+    
+            // validacao se ja existe usuario com o mesmo email
+            const usuariosComMesmoEmail = await usuarioModel.find({email : usuario.email});
+            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0){
+                return res.status(400).json({erro : 'Ja existe uma conta com o email informado'});
+            }
 
-      if (!usuario.nome || usuario.nome.length < 2) {
-        return res.status(400).json({ erro: "Nome inválido" });
-      }
-
-      if (
-        !usuario.email ||
-        usuario.email.length < 5 ||
-        !usuario.email.includes("@") ||
-        !usuario.email.includes(".")
-      ) {
-        return res.status(400).json({ erro: "Email inválido" });
-      }
-
-      if (!usuario.senha || usuario.senha.length < 4) {
-        return res.status(400).json({ erro: "senha inválida" });
-      }
-
-      // Validação se já existe usuario com o mesmo email
-      const usuarioComMesmoEmail = await usuarioModel.find({
-        email: usuario.email,
-      });
-
-      if (usuarioComMesmoEmail && usuarioComMesmoEmail.length > 0) {
-        return res
-          .status(400)
-          .json({ erro: "Já existe uma conta com esse email! " });
-      }
-
-      // enviar a imagem para o comic
-      const image = await uploadImagemCosmic(req)
-
-      //salvar no banco de dados
-      const usuarioAserSalvo = {
-        nome: usuario.nome,
-        email: usuario.email,
-        senha: md5(usuario.senha),
-        avatar: image?.media?.url 
-      };
-      await usuarioModel.create(usuarioAserSalvo);
-      return res.status(200).json({ msg: "Usuario criado com sucesso" });
+            // enviar a imagem do multer para o cosmic
+            const image = await uploadImagemCosmic(req);
+    
+            // salvar no banco de dados
+            const usuarioASerSalvo = {
+                nome : usuario.nome,
+                email : usuario.email,
+                senha : md5(usuario.senha),
+                avatar : image?.media?.url
+            }
+            await usuarioModel.create(usuarioASerSalvo);
+            return res.status(200).json({msg : 'Usuario criado com sucesso'});
+        }catch(e : any){
+            console.log(e);
+            return res.status(400).json({erro : e.toString()});
+        }
 });
 
 export const config = {
-  api:{
-    bodyParser : false
-  }
+    api: {
+        bodyParser : false
+    }
 }
 
 export default PoliticaCORS(conectarMongoDB(handler));
